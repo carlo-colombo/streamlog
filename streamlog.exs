20707fb655015a5dfd7c4a161f8e3004c89442af
Carlo Colombo <carlo.colombo@thoughtworks.com>
2024-10-09 19:08:59 +0200

    phx playground

--- streamlog.exs
+++ streamlog.exs
@@ -1,17 +1,10 @@
 Mix.install([
-  {:plug_cowboy, "~> 2.5"},
-  {:jason, "~> 1.0"},
-  {:phoenix, "~> 1.7", override: true},
-  {:phoenix_live_view, "~> 0.20"},
+  {:phoenix_playground, "~> 0.1.6"},
   {:phoenix_live_dashboard, "~> 0.8"}
 ])
 
-defmodule Streamlog.ErrorView do
-  def render(template, _), do: Phoenix.Controller.status_message_from_template(template)
-end
-
 defmodule Streamlog.IndexLive do
-  use Phoenix.LiveView, layout: {__MODULE__, :live}
+  use Phoenix.LiveView
 
   def mount(_params, _session, socket) do
     if connected?(socket) do
@@ -24,7 +17,10 @@ defmodule Streamlog.IndexLive do
       socket
       |> stream(:logs, filtered_lines(regex))
       |> assign(:form, to_form(%{"query" => query}))
-      |> assign(:title, Application.get_env(:streamlog, Streamlog.Endpoint) |> Keyword.get(:title))
+      |> assign(
+        :page_title,
+        Application.get_env(:streamlog, Streamlog.Endpoint) |> Keyword.get(:title)
+      )
 
     {:ok, response}
   end
@@ -65,30 +61,6 @@ defmodule Streamlog.IndexLive do
     |> Enum.sort(:desc)
   end
 
-  def render("live.html", assigns) do
-    ~H"""
-    <title><%= @title %></title>
-    <script src="https://cdn.jsdelivr.net/npm/phoenix@1.7.11/priv/static/phoenix.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/phoenix_live_view@0.20.12/priv/static/phoenix_live_view.min.js"></script>
-    <link rel="stylesheet" href="https://unpkg.com/mvp.css">
-    <script>
-      let liveSocket = new window.LiveView.LiveSocket("/live", window.Phoenix.Socket)
-      liveSocket.connect()
-    </script>
-    <style>
-      table td {
-        text-align: left;
-      }
-
-      #log-list tr {
-        font-family: monospace;
-      }
-
-    </style>
-    <%= @inner_content %>
-    """
-  end
-
   attr(:field, Phoenix.HTML.FormField)
   attr(:rest, :global, include: ~w(type))
 
@@ -113,11 +85,36 @@ defmodule Streamlog.IndexLive do
       </thead>
       <tbody id="log-list" phx-update="stream">
         <tr :for={{id, log} <- @streams.logs} id={id} >
-          <td><%= log.timestamp %></td>
-          <td><%= log.line %></td>
+          <td class="timestamp"><%= log.timestamp %></td>
+          <td class="message"><%= log.line %></td>
         </tr>
       </tbody>
     </table>
+    <link rel="stylesheet" href="https://unpkg.com/mvp.css">
+    <style>
+      :root {
+        --color-accent: #118bee15;
+      }
+      table {
+        text-align: left;
+        font-family: monospace;
+        display: table;
+        mi  n-width: 100%;
+
+        td {
+          padding-right: 10px;
+          border-right: solid 1px #339981;
+
+          &.message {
+            text-align: left;
+          }
+        }
+
+        tr:nth-child(even) {
+          background-color: var(--color-accent);
+        }
+      }
+    </style>
     """
   end
 end
@@ -129,12 +126,15 @@ defmodule Streamlog.Router do
 
   pipeline :browser do
     plug(:accepts, ["html"])
+    plug(:fetch_session)
+    plug(:put_root_layout, html: {PhoenixPlayground.Layout, :root})
+    plug(:put_secure_browser_headers)
   end
 
-  scope "/", Streamlog do
+  scope "/" do
     pipe_through(:browser)
 
-    live("/", IndexLive, :index)
+    live("/", Streamlog.IndexLive, :index)
     live_dashboard("/dashboard")
   end
 end
@@ -148,17 +148,20 @@ defmodule Streamlog.Worker do
     GenServer.start_link(__MODULE__, [], name: __MODULE__)
   end
 
+  @impl true
   def init([]) do
     {:ok, false}
   end
 
   def run() do
     GenServer.cast(__MODULE__, :run)
-    Phoenix.PubSub.subscribe(:lines, @topic)
+    Phoenix.PubSub.subscribe(PhoenixPlayground.PubSub, @topic)
   end
 
   @impl true
   def handle_cast(:run, false) do
+    IO.inspect(:run)
+
     :stdio
     |> IO.stream(:line)
     |> Stream.with_index(1)
@@ -175,14 +178,8 @@ defmodule Streamlog.Worker do
   def handle_cast(:run, :running), do: {:noreply, :running}
 
   defp notify_subscribers(line) do
-    Phoenix.PubSub.broadcast(:lines, @topic, {:line, line})
+    Phoenix.PubSub.broadcast(PhoenixPlayground.PubSub, @topic, {:line, line})
   end
-end
-
-defmodule Streamlog.Endpoint do
-  use Phoenix.Endpoint, otp_app: :streamlog
-  socket("/live", Phoenix.LiveView.Socket)
-  plug(Streamlog.Router)
 end
 
 defmodule Streamlog.State do
@@ -219,24 +216,21 @@ end
 {options, _, _} = OptionParser.parse(System.argv(), strict: [title: :string, port: :integer])
 
 Application.put_env(:streamlog, Streamlog.Endpoint,
-  http: [ip: {127, 0, 0, 1}, port: Keyword.get(options, :port, 5001)],
-  server: true,
-  live_view: [signing_salt: "aaaaaaaa"],
-  secret_key_base: String.duplicate("a", 64),
   title: Keyword.get(options, :title, "Stream Log")
 )
 
-{:ok, _} =
-  Supervisor.start_link(
-    [
-      Streamlog.Endpoint,
-      Streamlog.Worker,
-      {Phoenix.PubSub, name: :lines},
-      {Streamlog.State, %{"query" => nil}}
-    ],
-    strategy: :one_for_one
-  )
+{:ok, _} = Supervisor.start_link(
+  [
+  ],
+  strategy: :one_for_one
+)
 
-unless IEx.started?() do
-  Process.sleep(:infinity)
-end
+{:ok, _} = PhoenixPlayground.start(
+  plug: Streamlog.Router,
+  child_specs: [
+    Streamlog.Worker,
+    {Streamlog.State, %{"query" => nil}}
+  ],
+  open_browser: false,
+  port: Keyword.get(options, :port, 5001)
+)
